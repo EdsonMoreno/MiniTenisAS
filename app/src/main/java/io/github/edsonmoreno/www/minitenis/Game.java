b620705b77d87a5d756d64ea8e027d87a4157dbe
Edson D. Moreno S <diego2816@gmail.com>
2017-07-02 14:43:39 -0400

    Inicio el proyecto de minitenis en android studio

---
+++ app/src/main/java/io/github/edsonmoreno/www/minitenis/Game.java
@@ -0,0 +1,15 @@
+package io.github.edsonmoreno.www.minitenis;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+/**
+ * Created by Edson D.  on 02/07/2017.
+ */
+
+public class Game extends SurfaceView {
+    public Game (Context context){
+        super(context);
+    }
+
+}
