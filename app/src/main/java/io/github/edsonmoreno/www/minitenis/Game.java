75491651c1519e95a8602494220001c1e35e7579
Edson D. Moreno S <diego2816@gmail.com>
2017-07-02 15:08:46 -0400

    Creada estructura basica de lienzo

--- app/src/main/java/io/github/edsonmoreno/www/minitenis/Game.java
+++ app/src/main/java/io/github/edsonmoreno/www/minitenis/Game.java
@@ -1,7 +1,10 @@
 package io.github.edsonmoreno.www.minitenis;
 
 import android.content.Context;
+import android.graphics.Point;
+import android.view.Display;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 
 /**
  * Created by Edson D.  on 02/07/2017.
@@ -10,6 +13,22 @@ import android.view.SurfaceView;
 public class Game extends SurfaceView {
     public Game (Context context){
         super(context);
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display pantalla = wm.getDefaultDisplay();
+        Point point = new Point();
+        pantalla.getSize(point);
+
+        ancho = point.x;
+        alto = point.y;
     }
 
+    public void Actualizar(){
+
+    }
+
+    public void Pintar(){
+
+    }
+
+    private int ancho, alto;
 }
