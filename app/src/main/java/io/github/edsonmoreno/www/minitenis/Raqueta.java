5a94bb911264bac3b581ca43c367fb0a501c1a84
Edson D. Moreno S <diego2816@gmail.com>
2017-07-04 23:48:00 -0400

    Modificado algoritmo mover , la raqueta no se mueve sola inicializada variable x en 0 faltan validar los limites de la ventana se debe pulsar para que la raqueta se mueva

--- app/src/main/java/io/github/edsonmoreno/www/minitenis/Raqueta.java
+++ app/src/main/java/io/github/edsonmoreno/www/minitenis/Raqueta.java
@@ -13,38 +13,34 @@ public class Raqueta {
         xr=(game.getAncho()/2)-100;
         yr=game.getAlto()-70;
         raqueta = new RectF(xr,yr,xr+200,yr+30);
-        x=10;
+        x=0;
     }
 
     public RectF getRaqueta(){  return raqueta; }
 
-    private int dir=1;
+
     public void Actualizar(){
-        if(xr+x < game.getAncho()-200 && dir==1){
-            x=10;
-        }
-        if(xr+x >= game.getAncho()-200 && dir==1){
-            dir = 0;
-            x-=10;
-        }
-        if(xr+x > 0 && dir ==0){
-            x=-10;
-        }
-        if(xr <= 0){
-            dir = 1;
-            x+=10;
-        }
         xr+=x;
         this.raqueta.set(xr,yr,xr+200,yr+30);
     }
 
     public void Mover(MotionEvent event){
         float corx = event.getX();
-        if(corx < xr && dir == 1){
-            dir=0;
+        int action = event.getActionMasked();
+
+        switch (action){
+            case MotionEvent.ACTION_DOWN:
+                x=10;
+
+            case MotionEvent.ACTION_UP:
+                x=0;
+
         }
-        if(corx > (xr+200) && dir == 0){
-            dir=1;
+        if(corx < xr){
+            x*=1;
+        }
+        if(corx > (xr+200)){
+            x*=-1;
         }
     }
 
