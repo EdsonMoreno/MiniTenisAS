c764d4ce5ea05dca4ade5c6fb9413c43a2b400b5
Edson D. Moreno S <diego2816@gmail.com>
2017-07-05 11:49:56 -0400

    Bloqueada la vista del juego en vertical

--- app/src/main/java/io/github/edsonmoreno/www/minitenis/MainActivity.java
+++ app/src/main/java/io/github/edsonmoreno/www/minitenis/MainActivity.java
@@ -1,6 +1,7 @@
 package io.github.edsonmoreno.www.minitenis;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         game = new Game(this);
         setContentView(game);
+        //bloquea la actividad en vertical
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     private Game game;
