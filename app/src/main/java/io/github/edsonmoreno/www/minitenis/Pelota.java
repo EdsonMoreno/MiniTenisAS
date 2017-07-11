e864b3b61c4d5529bb435e436f4b19026f316057
Edson D. Moreno S <diego2816@gmail.com>
2017-07-11 19:24:22 -0400

    implementado  algoritmo de rebote aleatorio en el eje horizontal al rebotar con la raqueta

--- app/src/main/java/io/github/edsonmoreno/www/minitenis/Pelota.java
+++ app/src/main/java/io/github/edsonmoreno/www/minitenis/Pelota.java
@@ -12,31 +12,20 @@ public class Pelota {
         ancho=game.getAncho();
         alto=game.getAlto();
         xp=(ancho/2)-20;
-       // yp=(alto/2)-20;
-        yp=-100;
+        yp=-alto*2;
+        y=10;
         x=0;
-        y= 0;
-        velJuego=0;
-        golpe=0;
-        x=y=velJuego;
-        y=5;
-        x=5;
         pelota = new RectF(xp,yp,xp+40,yp+40);
-        movimiento_horizontal=false;
     }
 
     public RectF getPelota(){   return pelota;  }
 
     public void Actualizar(){
-        if(movimiento_horizontal) {
-            if (xp + x < 0) {
-                //  x=velJuego;
-                x = 10;
-            }
-            if (xp + x > game.getAncho() - 50) {
-                //x=-velJuego;
-                x = -10;
-            }
+        if (xp + x <= 0) {
+            x = 10;
+        }
+        if (xp + x > game.getAncho() - 50) {
+            x = -10;
         }
         if(yp+y < 0){
             y=10;
@@ -46,21 +35,23 @@ public class Pelota {
         }
         if(RectF.intersects(getPelota(),game.raqueta.getRaqueta())){
             y=-10;
-            movimiento_horizontal=true;
+            int num =(int) (Math.random()*2);
+            switch (num){
+                case 1:
+                    x=10;
+                    break;
+                case 2:
+                    x=-10;
+            }
+
         }
         xp+=x;
         yp+=y;
         pelota.set(xp, yp, xp+50, yp+50);
     }
 
-    public boolean Colision(RectF rectF) {
-        return pelota.intersect(rectF);
-    }
-
-    private int xp,yp,x,y, velJuego;
+    private int xp,yp,x,y;
     private int ancho, alto;
     private RectF pelota;
     private Game game;
-    private int golpe;
-    private boolean movimiento_horizontal;
 }
