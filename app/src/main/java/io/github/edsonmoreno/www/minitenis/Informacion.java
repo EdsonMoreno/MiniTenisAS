cc58adf1f6a0767dd287b8bce5ef562d36cca30c
Edson D. Moreno S <diego2816@gmail.com>
2017-07-14 23:31:54 -0400

    Programado archivo xmlcon texto de instrucciones agregado boton de volver programado codigo java para boton volver

--- app/src/main/java/io/github/edsonmoreno/www/minitenis/Informacion.java
+++ app/src/main/java/io/github/edsonmoreno/www/minitenis/Informacion.java
@@ -2,6 +2,7 @@ package io.github.edsonmoreno.www.minitenis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Informacion extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class Informacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion);
+    }
+
+    public void volver (View vista){
+        finish();
     }
 }
