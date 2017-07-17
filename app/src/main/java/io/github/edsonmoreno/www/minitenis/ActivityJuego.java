800617954a14364a21eed36dad046326194fa7e2
Edson D. Moreno S <diego2816@gmail.com>
2017-07-17 12:07:03 -0400

    Head

--- app/src/main/java/io/github/edsonmoreno/www/minitenis/ActivityJuego.java
+++ app/src/main/java/io/github/edsonmoreno/www/minitenis/ActivityJuego.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.widget.Toast;
 
 /**
  * Created by Diego on 15/07/2017.
@@ -28,7 +30,16 @@ public class ActivityJuego extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
+        System.out.println("onPause antes");
         game.Detener();
+        System.out.println("onPause Despues");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("DESPUES");
+        finish();
     }
 
     private Game game;
--- app/src/main/java/io/github/edsonmoreno/www/minitenis/Game.java
+++ app/src/main/java/io/github/edsonmoreno/www/minitenis/Game.java
@@ -6,17 +6,19 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 /**
  * Created by Edson D.  on 02/07/2017.
  */
 
 public class Game extends SurfaceView implements Runnable {
-    public Game (Context context){
+    public Game(Context context) {
         super(context);
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         Display pantalla = wm.getDefaultDisplay();
@@ -26,7 +28,7 @@ public class Game extends SurfaceView implements Runnable {
         holder = getHolder();
         paint = new Paint();
         paint.setColor(Color.WHITE);
-        score =getResources().getString(R.string.puntajes);
+        score = getResources().getString(R.string.puntajes);
         level = getResources().getString(R.string.nivel);
         life = getResources().getString(R.string.vidas);
         paint.setTextSize(30);
@@ -35,9 +37,9 @@ public class Game extends SurfaceView implements Runnable {
         alto = point.y;
 
         //calcula tres cuartos de pantalla para el texto d enivel
-        tres_cuartos = ancho/4;
-        un_cuarto = (ancho/4)+45;
-        tres_cuartos*=3;
+        tres_cuartos = ancho / 4;
+        un_cuarto = (ancho / 4) + 45;
+        tres_cuartos *= 3;
 
         nivel = 0;
         puntos = 0;
@@ -46,7 +48,7 @@ public class Game extends SurfaceView implements Runnable {
         raqueta = new Raqueta(this);
         pelota = new Pelota(this);
         pelotas = new Obstaculo[5];
-        for(int x=0; x<pelotas.length; x++){
+        for (int x = 0; x < pelotas.length; x++) {
             pelotas[x] = new Obstaculo(this);
         }
         pelotas[0].defineColor(Color.GREEN);
@@ -56,48 +58,102 @@ public class Game extends SurfaceView implements Runnable {
         pelotas[4].defineColor(Color.GRAY);
     }
 
-    public void Actualizar(){
-        raqueta.Actualizar();
-        pelota.Actualizar();
-        for(Obstaculo o:pelotas){
-            if(o.isVisible()) o.Actualizar();
+    public int getAncho() {
+        return ancho;
+    }
+
+    public int getAlto() {
+        return alto;
+    }
+
+    public void pierdeVida() {
+        vidas--;
+        if (vidas < 0) {
+            jugando = false;
         }
     }
 
-    public void Pintar(){
-        if(holder.getSurface().isValid()){
+    public void finDeJuego() {
+        nivel = 0;
+        puntos = 0;
+        vidas = 3;
+        jugando = false;
+    }
+
+    public void Actualizar() {
+        raqueta.Actualizar();
+        pelota.Actualizar();
+        for (Obstaculo o : pelotas) {
+            if (o.isVisible()) o.Actualizar();
+        }
+    }
+
+    public void Pintar() {
+        if (holder.getSurface().isValid()) {
             canvas = holder.lockCanvas();
             canvas.drawColor(getResources().getColor(R.color.Suelo));
             //Barra superior
             paint.setColor(Color.BLACK);
-            canvas.drawRect(0,0,ancho,100, paint);
+            canvas.drawRect(0, 0, ancho, 100, paint);
             //Resto del campo
             paint.setColor(Color.WHITE);
             canvas.drawRect(raqueta.getRaqueta(), paint);
             canvas.drawOval(pelota.getPelota(), paint);
-            for( int x=0; x < pelotas.length; x++ ){
+            for (int x = 0; x < pelotas.length; x++) {
                 paint.setColor(pelotas[x].colorPelota());
-                if(pelotas[x].isVisible()){
+                if (pelotas[x].isVisible()) {
                     canvas.drawRect(pelotas[x].getPelota(), paint);
                 }
             }
             paint.setColor(Color.WHITE);
-            canvas.drawText(score+" : "+puntos,2,70,paint);
-            canvas.drawText(level+" : "+nivel,tres_cuartos,70,paint);
-            canvas.drawText(life+": "+vidas, un_cuarto, 70, paint);
+            canvas.drawText(score + " : " + puntos, 2, 70, paint);
+            canvas.drawText(level + " : " + nivel, tres_cuartos, 70, paint);
+            canvas.drawText(life + ": " + vidas, un_cuarto, 70, paint);
             holder.unlockCanvasAndPost(canvas);
         }
     }
 
-    public int getAncho(){  return ancho;   }
-    public int getAlto(){   return alto;    }
-    public void pierdeVida(){   vidas--;    }
-    public boolean estaMuerto(){
-        if(vidas > 0){
+    public boolean estaMuerto() {
+        if (vidas >= 0) {
             return true;
-        }else {
+        } else {
             return false;
         }
+    }
+
+    public void Detener() {
+        if(jugando == false){
+            System.out.println("ANTES");
+            return;
+        }
+        System.out.println("DESPUES");
+        jugando = false;
+        try {
+            pintor.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void SumarPuntos() {
+        puntos++;
+        if (puntos % 5 == 0) {
+            pelota.CambiarVelocidad();
+            nivel++;
+
+            if (pelotas[3].isVisible()) pelotas[4].mostrarCuadro();
+            if (pelotas[2].isVisible()) pelotas[3].mostrarCuadro();
+            if (pelotas[1].isVisible()) pelotas[2].mostrarCuadro();
+            if (pelotas[0].isVisible()) pelotas[1].mostrarCuadro();
+            pelotas[0].mostrarCuadro();
+        }
+    }
+
+    public void nuevoJuego() {
+        if (vidas < 0) this.finDeJuego();
+        jugando = true;
+        pintor = new Thread(this);
+        pintor.start();
     }
 
     @Override
@@ -108,57 +164,29 @@ public class Game extends SurfaceView implements Runnable {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        while(jugando){
-            long startFrameTime = System.currentTimeMillis();
+        while (jugando) {
+          //  long startFrameTime = System.currentTimeMillis();
             Actualizar();
             Pintar();
-            timeThisFrame = System.currentTimeMillis() - startFrameTime;
+            System.out.println("BUCLEEEEEEEEEE");
+         /*   timeThisFrame = System.currentTimeMillis() - startFrameTime;
             if (timeThisFrame >= 1) {
                 fps = 1000 / timeThisFrame;
-            }
+            }*/
         }
-
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         raqueta.Mover(event);
         int a = event.getActionMasked();
-        switch (a){
+        switch (a) {
             case MotionEvent.ACTION_DOWN:
                 return true;
             case MotionEvent.ACTION_UP:
                 return true;
         }
         return super.onTouchEvent(event);
-    }
-
-    public void Detener(){
-        jugando=false;
-        try {
-            pintor.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-    public void SumarPuntos(){
-        puntos++;
-        if(puntos % 5 == 0){
-            pelota.CambiarVelocidad();
-            nivel++;
-
-            if(pelotas[3].isVisible())   pelotas[4].mostrarCuadro();
-            if(pelotas[2].isVisible())   pelotas[3].mostrarCuadro();
-            if(pelotas[1].isVisible())   pelotas[2].mostrarCuadro();
-            if(pelotas[0].isVisible())   pelotas[1].mostrarCuadro();
-            pelotas[0].mostrarCuadro();
-        }
-    }
-
-    public void nuevoJuego(){
-        jugando=true;
-        pintor = new Thread(this);
-        pintor.start();
     }
 
     private int ancho, alto, puntos, nivel, vidas, tres_cuartos, un_cuarto;
