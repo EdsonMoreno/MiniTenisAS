e3b75c90994a02fa04e42e7dba500514fe0493db
Edson D. Moreno S <diego2816@gmail.com>
2017-07-19 00:01:04 -0400

    Implementado algoritmo de inserciondirecta para ordenar array de ganadores

--- app/src/main/java/io/github/edsonmoreno/www/minitenis/Galeria.java
+++ app/src/main/java/io/github/edsonmoreno/www/minitenis/Galeria.java
@@ -14,6 +14,10 @@ public class Galeria extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galeria);
 
+        ganadores = new int[5];
+        for(Integer g:ganadores){
+            g = 0;
+        }
         TextView texto = (TextView) findViewById(R.id.p1);
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         int aux = sharedPreferences.getInt("puntos",0);
@@ -21,24 +25,46 @@ public class Galeria extends AppCompatActivity {
     }
 
     public void Salir(View vista){
+        insercionDirecta(ganadores);
+        salvarPuntajes();
         finish();
     }
-    public void ordenarQuicksort(int[] vector, int primero, int ultimo){
-        int i=primero, j=ultimo;
-        int pivote=vector[(primero + ultimo) / 2];
-        int auxiliar;
-        do{
-            while(vector[i]<pivote) i++;
-            while(vector[j]>pivote) j--;
-            if (i<=j){
-                auxiliar=vector[j];
-                vector[j]=vector[i];
-                vector[i]=auxiliar;
-                i++;
-                j--;
-            }
-        } while (i<=j);
-        if(primero<j) ordenarQuicksort(vector,primero, j);
-        if(ultimo>i) ordenarQuicksort(vector,i, ultimo);
+
+    private void salvarPuntajes(){
+        //salvamos los cinco puntahes en orden
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editable = sharedPreferences.edit();
+        editable.putInt("uno",ganadores[0]);
+        editable.putInt("dos",ganadores[1]);
+        editable.putInt("tres",ganadores[2]);
+        editable.putInt("cuatro",ganadores[3]);
+        editable.putInt("cinco",ganadores[4]);
+        editable.apply();
     }
+
+    private void  obtenerPuntajes(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        ganadores[0] = sharedPreferences.getInt("uno",0);
+        ganadores[1] = sharedPreferences.getInt("dos",0);
+        ganadores[2] = sharedPreferences.getInt("tres",0);
+        ganadores[3] = sharedPreferences.getInt("cuatro",0);
+        ganadores[4] = sharedPreferences.getInt("cinco",0);
+    }
+
+    public static void insercionDirecta(int A[]){
+        int p, j;
+        int aux;
+        for (p = 1; p < A.length; p++){ // desde el segundo elemento hasta
+            aux = A[p]; // el final, guardamos el elemento y
+            j = p - 1; // empezamos a comprobar con el anterior
+            while ((j >= 0) && (aux < A[j])){ // mientras queden posiciones y el
+                // valor de aux sea menor que los
+                A[j + 1] = A[j];       // de la izquierda, se desplaza a
+                j--;                   // la derecha
+            }
+            A[j + 1] = aux; // colocamos aux en su sitio
+        }
+    }
+
+    private int ganadores[];
 }
